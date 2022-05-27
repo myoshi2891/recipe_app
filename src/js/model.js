74cef05f300c797f7c3edcd56494228f0995a7a9
mitsuruyoshizumi <mitsuruyoshizumi@yahoo.co.jp>
2022-05-28 02:29:28 +0900

    Implement search result

--- src/js/model.js
+++ src/js/model.js
@@ -11,11 +11,15 @@ import {
 
 export const state = {
   recipe: {},
+  search: {
+    query: '',
+    results: [],
+  }
 };
 
 export const loadRecipe = async function (id) {
   try {
-    const data = await getJSON(`${API_URL}/${id}`);
+    const data = await getJSON(`${API_URL}${id}`);
 
     const {
       recipe
@@ -30,9 +34,31 @@ export const loadRecipe = async function (id) {
       cookingTime: recipe.cooking_time,
       ingredients: recipe.ingredients
     }
-    console.log(state.recipe);
+    // console.log(state.recipe);
   } catch (err) {
     console.error(`${err}🤬🤬🤬`);
     throw err;
   }
 };
+
+export const loadSearchResults = async function (query) {
+  try {
+    state.search.query = query;
+
+    const data = await getJSON(`${API_URL}?search=${query}`)
+    console.log(data);
+
+    state.search.results = data.data.recipes.map(rec => {
+      return {
+        id: rec.id,
+        title: rec.title,
+        publisher: rec.publisher,
+        image: rec.image_url,
+      }
+    })
+    console.log(state.search.results);
+  } catch (err) {
+    console.error(`${err}🤬🤬🤬`);
+    throw err;
+  }
+}
